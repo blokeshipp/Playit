6c2aeb7861a516729d724eb20e8d6d68af77c29b
blokeshipp <shippingbloke@gmail.com>
2025-08-26 07:18:22 +0200

    Update playit.sh

--- playit.sh
+++ playit.sh
@@ -1,4 +1,11 @@
+# Importar la clave GPG y guardarla en el directorio de llaves de APT
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+
+# Añadir el repositorio de Playit
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+
+# Actualizar índices de paquetes
 sudo apt update
-sudo apt install playit
+
+# Instalar playit
+sudo apt install -y playit
